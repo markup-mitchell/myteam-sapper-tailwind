1adcc5a1bf8ae827b4999d0de97c6322f0ae2e87
spitchell <mr.spitchell@gmail.com>
2020-08-18 14:00:50 +0100

    mobile about section 1

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,24 @@
+<script>
+	import StandardPadding from "../components/StandardPadding.svelte";
+	let bg1 = "images/bg-pattern-about-1-mobile-nav-1.svg";
+</script>
+
 <svelte:head>
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<div class="relative">
+
+	<div class="text-center text-white pt-20 pb-32">
+		<StandardPadding>
+			
+			<h1>About</h1>
+			<p class="mt-4 tracking-wide leading-7">
+				We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring those teams to you.
+			</p>
+		</StandardPadding>
+	</div>
+	<img src={bg1} alt="" class="w-48 h-48 absolute bottom-0 right-0 -mb-24 -mr-24">
+	
+</div>
