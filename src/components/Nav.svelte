4d9214481c9c5e49df5ad3dbbf3b19218501777b
spitchell <mr.spitchell@gmail.com>
2020-08-18 12:09:14 +0100

    lock screen when sidebar open

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,11 +1,13 @@
 <script>
+	import { fly,fade } from 'svelte/transition';
 import BtnLinkContactUs from "./BtnLinkContactUs.svelte";
 	let logo = "images/logo.svg";
 	let close = "images/icon-close.svg";
 	let menu = "images/icon-hamburger.svg";
 	export let segment;
-	let sidebarOpen = true;
-	$: if (process.browser) document.body.classList.toggle('overflow-y-hidden', sidebarOpen)
+	let sidebarOpen = false;
+	$: if (process.browser) document.body.classList.toggle('overflow-y-hidden', sidebarOpen);
+	$: if (process.browser) document.body.classList.toggle('overflow-y-scroll', !sidebarOpen);
 </script>
 
 <nav class="flex justify-between px-6 pt-12">
@@ -15,24 +17,24 @@ import BtnLinkContactUs from "./BtnLinkContactUs.svelte";
 
 	
 	{#if sidebarOpen}
-		<div class="absolute inset-0 bg-black opacity-50 z-10"></div>
+		<div transition:fade class="absolute inset-0 bg-black opacity-50 z-10"></div>
 		
-	<div class="absolute top-0 right-0 inset-y-0 bg-red-400 z-10 pt-12 px-6">
+	<div transition:fly="{{ x: 200, duration: 400 }}" class="absolute top-0 right-0 inset-y-0 bg-s2 z-10 pt-12 pl-12 pr-6 w-64">
 <div class="flex flex-col items-end">
 	<button class="h-10" on:click={()=>sidebarOpen = !sidebarOpen}>
 		<img src={close} alt="">
 	</button>
-	<ul class="text-white text-xl flex flex-col items-start w-full mt-8">
-		<li ><a  href='/'>home</a></li>
-		
+	<div class=" flex flex-col items-start w-full mt-8">
+		<ul class="text-white text-xl">
+			<li ><a  href='/'>home</a></li>
 			<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 				the blog data when we hover over the link or tap it on a touchscreen -->
 				<li class="mt-6"><a rel=prefetch href='about'>about</a></li>
 			</ul>
 			<div class="mt-10">
-
 				<BtnLinkContactUs/>
 			</div>
+		</div>
 		</div>
 		</div>
 		{/if}
