88d27abb77fdac5c64056c32c7bd7f30a733a939
spitchell <mr.spitchell@gmail.com>
2020-08-19 18:26:32 +0100

    purgecss exceptions fot prugecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
-const tailwindcss = require("tailwindcss");
+const tailwindcss = require('tailwindcss');
 
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.svelte", "./src/**/*.html"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.svelte', './src/**/*.html'],
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ['rotate-45', 'hover:bg-s1', 'hover:bg-p2']
 });
 
 module.exports = {
   plugins: [
-    tailwindcss("./tailwind.config.js"),
+    tailwindcss('./tailwind.config.js'),
 
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
   ]
 };
